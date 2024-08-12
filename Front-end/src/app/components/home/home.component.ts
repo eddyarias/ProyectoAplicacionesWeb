23ee5f63f7370c8577287a344cc3668fc704b2ce
Milton599 <94476149+Milton599@users.noreply.github.com>
2024-08-12 18:29:09 -0500

    reseñas home

--- Front-end/src/app/components/home/home.component.ts
+++ Front-end/src/app/components/home/home.component.ts
@@ -5,6 +5,10 @@ import { Global } from '../../services/global';
 import { CommonModule } from '@angular/common';
 import { HttpClientModule } from '@angular/common/http';
 import { RouterModule } from '@angular/router';
+import { ReviewService } from '../../services/review.service';
+import { response } from 'express';
+import { Review } from '../../modules/review';
+import { UserService } from '../../services/user.service';
 
 @Component({
   selector: 'app-home',
@@ -12,35 +16,59 @@ import { RouterModule } from '@angular/router';
   imports: [CommonModule, HttpClientModule, RouterModule ],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css',
-  providers: [JuegoService]
+  providers: [JuegoService, ReviewService, UserService]
   
 })
 export class HomeComponent implements OnInit{
-  public juegos:Juego[];
   public url:string;
   public juegosPopulares:Juego[];
+  public reviews:Review[];
 
   constructor(
-    private _servicePelicula:JuegoService
+    private _servicePelicula:JuegoService,
+    private _serviceReview:ReviewService,
+    private _serviceUsuario:UserService
   ){
-    this.juegos = [];
     this.url = Global.url;
     this.juegosPopulares = [];
+    this.reviews = [];
   }
   
   ngOnInit(): void {
     this.getGames();
+    this.getReviews();
   }
 
   getGames(){
     this._servicePelicula.getGames().subscribe(
       response => {
         if(response.games){
-          this.juegos = response.games;
-          this.juegosPopulares = this.juegos.slice(0,8);
+          this.juegosPopulares = response.games.slice(0,8);
         }
       },
       error => {  
+        console.log(error);
+      }
+    )
+  }
+
+  getReviews(){
+    this._serviceReview.getReviews().subscribe(
+      response => {
+        if(response.reviews){
+          this.reviews = response.reviews.slice(0,3);
+
+          this.reviews.forEach(review => {
+            this._serviceUsuario.getUser(review.user_id).subscribe(
+              response => {
+                review['userName'] = response.user.nombre;
+                review['userImage'] = response.user.imagen;
+              }
+            )
+          });
+        }
+      },
+      error => {
         console.log(error);
       }
     )
