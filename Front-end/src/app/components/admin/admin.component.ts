562181dcf90ed258023fab1e3a08b4b3e64fb793
jonathanpoaquiza <109117858+jonathanpoaquiza75@users.noreply.github.com>
2024-08-15 02:59:32 -0500

    Fix admin button

--- Front-end/src/app/components/admin/admin.component.ts
+++ Front-end/src/app/components/admin/admin.component.ts
@@ -1,16 +1,16 @@
 import { Component } from '@angular/core';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { RouterModule, RouterOutlet } from '@angular/router';
 import { CrearJuegoComponent } from "../crear-juego/crear-juego.component";
 import { BorrarJuegoComponent } from '../borrar-juego/borrar-juego.component';
 import { ActualizarJuegoComponent } from '../actualizar-juego/actualizar-juego.component';
 import { LeerJuegoComponent } from '../leer-juegos/leer-juegos.component';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-admin',
   standalone: true,
   imports: [RouterOutlet,
-    BrowserAnimationsModule,
+    CommonModule,
     RouterModule, CrearJuegoComponent, BorrarJuegoComponent, ActualizarJuegoComponent, LeerJuegoComponent],
   templateUrl: './admin.component.html',
   styleUrl: './admin.component.css'
